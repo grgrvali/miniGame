d36c691563320062aa69c170e5e987c67dcfc361
grgrvali <109388102+grgrvali@users.noreply.github.com>
2022-12-01 10:50:17 +0300

    Add files via upload

---
+++ game.js
@@ -0,0 +1,29 @@
+const board = document.querySelector("#board");
+const colors = ["#da70d6", "#ffb6c1d", "#FF00FF", "#9370db", "#dda0dd"];
+const SQUARES_NUMBER = 400;
+
+for (let i = 0; i < SQUARES_NUMBER; i++) {
+  const square = document.createElement("div");
+  square.classList.add("square");
+
+  square.addEventListener("mouseover", () => setColor(square));
+  square.addEventListener("mouseleave", () => removeColor(square));
+
+  board.append(square);
+}
+
+function setColor(element) {
+  const color = getRandomColor();
+  element.style.backgroundColor = color;
+  element.style.boxShadow = `0 0 2px ${color}, 0 0 15px ${color}`;
+}
+
+function removeColor(element) {
+  element.style.backgroundColor = "#1d1d1d";
+  element.style.boxShadow = `0 0 2px #000`;
+}
+
+function getRandomColor() {
+  const index = Math.floor(Math.random() * colors.length);
+  return colors[index];
+}
